a1dcdf83a32f0aa073955f96f5b23a7ce3e6ccdb
JJ1312 <yeiyeithirteen@gmail.com>
2024-02-25 18:47:02 -0300

    Agrega cambio color navbar al hacer scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,9 +3,18 @@ document.addEventListener('DOMContentLoaded', ()=>{
   const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
   const btnSubmit = document.querySelector('#btn-submit');
-
   btnSubmit.addEventListener('click', (event) => {
     event.preventDefault();
     alert('Mensaje enviado con exito');
+  });
+
+  const navbar = document.querySelector('nav');
+  document.addEventListener('scroll', () => {
+    if(window.scrollY > 500){
+      navbar.setAttribute('data-bg', 'fill');
+    }
+    else{
+      navbar.setAttribute('data-bg', 'transparent')
+    }
   });
 });
